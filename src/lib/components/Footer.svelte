1d9ea1cbda64788b2b6e2e97375b0210dccd6d37
elijah <elijahkim45@gmail.com>
2026-06-13 16:13:31 -0700

    feat: footer implemented

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,24 @@
+<footer class="footer">
+  <div class="container footer-inner">
+    <p>&copy; {new Date().getFullYear()} Meyer Distributing</p>
+  </div>
+</footer>
+
+<style lang="scss">
+  .footer {
+    border-top: 1px solid $color-border;
+    background-color: $color-bg-main;
+    color: $color-text-muted;
+  }
+
+  .footer-inner {
+    padding: 20px 16px;
+    font-size: 14px;
+    line-height: 1.4;
+    text-align: center;
+  }
+
+  .footer-inner p {
+    margin: 0;
+  }
+</style>
